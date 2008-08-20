de56bbbeb7bf020dd5bb3530d1fb019f5d2808fa
Alex Sharp <ajsharp@gmail.com>
2008-08-20 00:14:04 -0500

    initial commit

---
+++ lib/list_sorter.rb
@@ -0,0 +1,26 @@
+module ListSorter
+  # alias_method :active_sort, :collection_sort
+  
+  # use when the parent is not an active record object.
+  # def normal_sort(params)
+  #   params[:child].each do |item|
+  #     item.position = params["#{options[:index]}"].index(item.id.to_s) + 1
+  #   end
+  # end
+  
+  # This uses the method described in recipe 5 of "Rails Recipes"
+  # 
+  # options
+  # parent is an instance of the parent model
+  # children is a symbol of the collection to be sorted
+  def collection_sort(params)
+    options = {}
+    parent = eval(params[:parent].capitalize).find(params[:id])
+    options[:index] = params[:index] || children.to_s
+     
+    parent.send(params[:children]).each do |child|
+      child.position = params["#{options[:index]}"].index(child.id.to_s) + 1
+      child.save
+    end
+  end
+end
